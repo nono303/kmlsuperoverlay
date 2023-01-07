9d165cc8e8c3400f1defc0b066260d8805914393
nono303 <git@nono303.net>
2023-01-07 19:35:15 +0100

    add composer.json and fix index.sample.php to correctly load Brick\Geo component

--- index.sample.php
+++ index.sample.php
@@ -4,11 +4,19 @@
 	include_once(__DIR__ ."/class/Gis.php");
 	include_once(__DIR__ ."/class/Common.php");
 	include_once(__DIR__ ."/class/KmlSuperOverlay.php");
-
+	if(extension_loaded("geos")){
+		if(!class_exists("Brick\Geo\Engine\GEOSEngine")){
+			if(is_file(__DIR__ ."/vendor/autoload.php")) {
+				include_once(__DIR__ ."/vendor/autoload.php");
+			} else {
+				throw new Exception("'geos' extension loaded but 'Brick\Geo' missing. Please run composer install");
+			}
+		}
+	}
 	// !! with ending '/'
 	define("MAP_SOURCE_ROOT","./mapsources/");
 	// !! with ending '/'
-	define("URL_BASE","/kmlsuperoverlays/");
+	define("URL_BASE","/kmlsuperoverlay/");
 	// enable (slow & higher memory usage) or disable kml clean & format with tidy extension
 	define("TIDY_KML",false);
 	// enable kml indent if TIDY_KML == false
